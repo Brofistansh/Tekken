65fa9fcaabbb262630d8644c4bae30723614e5fa
Brofistansh <jaiansh2016@gmail.com>
2023-06-09 22:30:15 +0530

    animation for player1 is done including jumping

---
+++ Assets/scripts/player1actions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1actions : MonoBehaviour
+{
+    public float JumpSpeed = 1.0f;
+    public GameObject Player1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void JumpUp()
+    {
+        Player1.transform.Translate(0, JumpSpeed, 0);
+    }
+}
--- Assets/scripts/player1move.cs
+++ Assets/scripts/player1move.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 public class player1move : MonoBehaviour
 {
     private Animator Anim;
+    public float WalkSpeed = 0.001f;
+    public float JumpSpeed = 1.0f;
+    private bool IsJumping = false;
+    private AnimatorStateInfo Player1Layer0;
+
     // Start is called before the first frame update
     void Start()
     {
-        Anim=GetComponent<Animator>();
+        Anim=GetComponentInChildren<Animator>();
          
     }
 
     // Update is called once per frame
     void Update()
     {
+        Player1Layer0 = Anim.GetCurrentAnimatorStateInfo(0);
         //walking forward and backward
-        if(Input.GetAxis("Horizontal") >0)
+        if (Player1Layer0.IsTag("Motion"))
         {
-            Anim.SetBool("forward", true);
-        }
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            Anim.SetBool("backward", true);
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                Anim.SetBool("forward", true);
+                transform.Translate(WalkSpeed, 0, 0);
+            }
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                Anim.SetBool("backward", true);
+                transform.Translate(-WalkSpeed, 0, 0);
+            }
         }
         if (Input.GetAxis("Horizontal") == 0)
         {
@@ -33,7 +44,13 @@ public class player1move : MonoBehaviour
         //jumping and crouching
         if (Input.GetAxis("Vertical") > 0)
         {
-            Anim.SetTrigger("jump");
+            if (IsJumping == false)
+            {
+                IsJumping = true;
+                Anim.SetTrigger("jump");
+                StartCoroutine(JumpPause());
+            }
+
         }
         if (Input.GetAxis("Vertical") < 0)
         {
@@ -44,5 +61,11 @@ public class player1move : MonoBehaviour
             Anim.SetBool("crouch", false);
         }
 
+    }
+
+    IEnumerator JumpPause()
+    {
+        yield return new WaitForSeconds(1.0f);
+        IsJumping = false;
     }
 }
